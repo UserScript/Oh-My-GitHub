d5974916010882f21d38e72cc14841ab41abc305
cssmagic <cssmagic.cn@gmail.com>
2015-08-28 20:35:53 +0800

    Update URL.

--- src/meta.js
+++ src/meta.js
@@ -5,8 +5,8 @@
 // @namespace       https://github.com/UserScript
 // @homepage        https://github.com/UserScript/Oh-My-GitHub
 // @description     Make GitHub even better.
-// @downloadURL     https://rawgit.com/UserScript/Oh-My-GitHub/master/dist/oh-my-github.user.js
-// @updateURL       https://rawgit.com/UserScript/Oh-My-GitHub/master/src/meta.js
+// @downloadURL     https://raw.githubusercontent.com/UserScript/Oh-My-GitHub/master/dist/oh-my-github.user.js
+// @updateURL       https://raw.githubusercontent.com/UserScript/Oh-My-GitHub/master/src/meta.js
 // @icon            https://github.com/favicon.ico
 // @grant           none
 // @noframes
